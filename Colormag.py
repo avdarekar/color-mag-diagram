9701a0810433f5e57946308a93a5cf40f70fbda8
Ayesha Darekar <62392092+avdarekar@users.noreply.github.com>
2025-07-25 21:58:27 -0700

    cleaned code, refactored code so it is more effecient, updated README, created config.py file, created requirements.txt file

--- Colormag.py
+++ Colormag.py
@@ -1,72 +1,79 @@
-#import xlrd, matplotlib.pyplot, and math libraries
-import xlrd
 import matplotlib.pyplot as plt
-import math 
-from xlrd import open_workbook
+import pandas as pd
+import numpy as np
 
-#open Gaia data .xlsx file from computer
-bench = open_workbook('/Users/adbreeze13/Desktop/UNCResearch/Test/finaldata.xlsx',on_demand=True)
-
-#declare arrays for apparent magnitude, bp-rp, parallax, parallax over error, g mean flux over error, bp flux over error, rp flux over error, and absolute magnitude respectively
-appmagcol1 = []
-bp_rpcol2 = []
-parallaxcol3 = []
-poecol4 = []
-gfloecol5 = []
-bpfloecol6 = []
-rpfloecol7 = []
-absmag = []
-
-#iterate over each row in .xlsx file
-for name in bench.sheet_names():
-    sheetnew = bench.sheet_by_name(name)
-    for row in range(0, 100000):
-        
-        #assign type of cell in each column to corresponding column variable   
-        appmagcoltype = sheetnew.cell_type(row, 0)
-        bp_rpcoltype = sheetnew.cell_type(row, 1)
-        parallaxcoltype = sheetnew.cell_type(row, 2)
-        poecoltype = sheetnew.cell_type(row, 3)
-        gfloecoltype = sheetnew.cell_type(row, 4)
-        bpfloecoltype = sheetnew.cell_type(row, 5)
-        rpfloecoltype = sheetnew.cell_type(row, 6)
-        
-        #if cells in all columns are not empty 
-        if not appmagcoltype == xlrd.XL_CELL_EMPTY and not bp_rpcoltype == xlrd.XL_CELL_EMPTY and not parallaxcoltype == xlrd.XL_CELL_EMPTY and not poecoltype == xlrd.XL_CELL_EMPTY and not gfloecoltype == xlrd.XL_CELL_EMPTY and not bpfloecoltype == xlrd.XL_CELL_EMPTY and not rpfloecoltype == xlrd.XL_CELL_EMPTY:
-            #if cell values in parallax over error, g mean flux over error, bp flux over error, and rp flux over error columns are above 20, append cell values in each column to corresponding column array
-            if sheetnew.cell_value(row, 3) > 20 and sheetnew.cell_value(row, 4) > 20 and sheetnew.cell_value(row, 5) > 20 and sheetnew.cell_value(row,6) > 20:
-                appmagcol1.append(sheetnew.cell_value(row, 0))
-                bp_rpcol2.append(sheetnew.cell_value(row, 1))
-                parallaxcol3.append(sheetnew.cell_value(row,2))
-                poecol4.append(sheetnew.cell_value(row, 3))
-                gfloecol5.append(sheetnew.cell_value(row, 4))
-                bpfloecol6.append(sheetnew.cell_value(row, 5))
-                rpfloecol7.append(sheetnew.cell_value(row, 6))
-
-        
-
-        
-
-#convert apparent magnitude to absolute magnitude and append these values to absmag array
-for i in range(len(appmagcol1)):
-    abscalc = appmagcol1[i]-(5*math.log((1000/parallaxcol3[i]), 10))+5
-    absmag.append(abscalc)
+from config import file_path
 
 
+def load_data() -> pd.DataFrame:
+    """
+    Load Gaia data.
+
+    Returns:
+        data: DataFrame containing Gaia data
+    """
+    data = pd.read_csv(file_path)
+    return data
 
 
-#create scatter plot - bp-rp on x-axis, absolute magnitude on y-axis, invert the y-axis, size of marker is .01        
-plt.scatter(bp_rpcol2, absmag, s=.01)
-plt.xlabel('Bp-Rp')
-plt.ylabel('Absolute Magnitude')
-plt.gca().invert_yaxis()
-plt.title('Color-Magnitude Diagram')
-plt.show()
+def subset_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Subset Gaia data so it only includes data relevant for plotting.
+
+    Arguments:
+        data: DataFrame containing Gaia data
+
+    Returns:
+        subsetted_data: subsetted DataFrame containing Gaia data
+    """
+
+    subsetted_data = data.dropna()
+    subsetted_data = subsetted_data.loc[
+        (subsetted_data["parallax_over_error"] > 20)
+        & (subsetted_data["phot_g_mean_flux_over_error"] > 20)
+        & (subsetted_data["phot_bp_mean_flux_over_error"] > 20)
+        & (subsetted_data["phot_rp_mean_flux_over_error"] > 20)
+    ]
+
+    return subsetted_data
 
 
+def convert_to_absolute_magnitude(
+    apparent_magnitude: pd.Series, parallax: pd.Series
+) -> pd.Series:
+    """
+    Convert apparent magnitude to absolute magnitude.
+
+    Arguments:
+        apparent_magnitude: pandas Series containing apparent magnitudes
+        parallax: pandas Series containing parallax values
+
+    Returns:
+        absolute_magnitude: pandas Series containing absolute magnitudes
+    """
+    absolute_magnitude = apparent_magnitude - (5 * np.log10((1000 / parallax))) + 5
+
+    return absolute_magnitude
 
 
+def plot_color_magnitude():
+    """
+    Create color magnitude plot using Gaia data.
+    """
+    gaia_data = load_data()
+    gaia_data = subset_data(gaia_data)
+
+    absolute_magnitude = convert_to_absolute_magnitude(
+        gaia_data["phot_g_mean_mag"], gaia_data["parallax"]
+    )
+
+    plt.scatter(gaia_data["bp_rp"], absolute_magnitude, s=0.01)
+    plt.xlabel("Bp-Rp")
+    plt.ylabel("Absolute Magnitude")
+    plt.gca().invert_yaxis()
+    plt.title("Color-Magnitude Diagram")
+    plt.show()
 
 
-
-
+# how to run run script:
+plot_color_magnitude()
--- README.md
+++ README.md
@@ -9,8 +9,9 @@ This program uses data from the [Gaia archive](https://gea.esac.esa.int/archive/
 * Usage
 
 ## File Descriptions
-* Colormag.py: this is the Python program I wrote to create the color-magnitude diagram
-* Colormag_diagram.png: this is a screenshot of the color-magnitude diagram that Colormag.py created
+* colormag.py: this is the Python program I wrote to create the color-magnitude diagram
+* colormag_diagram.png: this is a screenshot of the color-magnitude diagram that Colormag.py created
+* config.py: this is a config file where the user specifies the local filepath to the Gaia dataset
 
 ## Obtaining Data
 Data was obtained from the [Gaia archive](https://gea.esac.esa.int/archive/). This SQL statement was used in the Gaia archive to extract the data:
@@ -24,16 +25,13 @@ WHERE (gaiadr2.gaia_source.parallax>=10)
 The Gaia archive allowed me to download this data as a csv file.
 
 ## Installation 
-To successfully run this program on your local computer:
+To successfully run this program locally:
 1. Run the SQL statement above in the [Gaia archive](https://gea.esac.esa.int/archive/). 
 2. Download the data as a csv file. 
-3. Save the csv file as a .xlsx file. 
-4. Download the Colormag.py file to your computer.
-5. Install xlrd, matplotlib.pyplot, and math python libraries.
-6. Replace the location of the .xlsx file with the location of the .xlsx file on your computer. 
+3. Clone this repo and install requirements in a virtual environment using the requirements.txt file. 
+4. Add the filepath to the dataset in config.py. 
 
 ## Usage
-After installation, run the program. Keep in mind that it will take some time for the program to run due to the large amount of data.
-
+After installation, run the plot_color_magnitude() function in colormag.py.
 
 
